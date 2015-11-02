c052b523bcf5701240925dab43703e362f6b20bf
akella <akella.a@gmail.com>
2015-11-02 18:15:37 +0200

    gogo

--- src/js/common.js
+++ src/js/common.js
@@ -1,19 +1,23 @@
 $(document).ready(function() {
-	
-	// $(document).on("click", function(){
-	// 	$(".js-popup").hide();
-	// });
-
-	// function scrollFixedElements() {
-	//     var scroll_left = $(this).scrollLeft();
-	//     $(".fixed-element").css({
-	//         left: -scroll_left
-	//     });
-	// }
-	// scrollFixedElements();
-	// $(window).scroll(function(){
-	//     scrollFixedElements()
-	// });
-
-	console.log($('body').html());
+	$('.js-menu').click(function(event) {
+		$('.header__drop').toggle();
+	});
+	$('.js-slider').slick({
+		slidesToShow: 1,
+	  	slidesToScroll: 1,
+	  	infinite: true,
+		arrows: true,
+		dots: true,
+		prevArrow: $('.topslider__prev'),
+		nextArrow: $('.topslider__next')
+	})
+	$('.js-promo').slick({
+		slidesToShow: 3,
+	  	slidesToScroll: 1,
+	  	infinite: true,
+		arrows: true,
+		dots: false,
+		prevArrow: $('.promo__prev'),
+		nextArrow: $('.promo__next')
+	})
 });
