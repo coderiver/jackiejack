7177f4050bb189ec5bed1ed616ed2852f5ac0b2d
akella <akella.a@gmail.com>
2015-11-25 15:37:04 +0200

    update carousels

--- site/js/app.js
+++ site/js/app.js
@@ -23,30 +23,39 @@ return new Za.prototype.init(a,b,c,d,e)}m.Tween=Za,Za.prototype={constructor:Za,
 })):i.postSlide(e),i.animateHeight(),void 0):(c!==!0?i.animateSlide(h,function(){i.postSlide(e)}):i.postSlide(e),void 0)))},b.prototype.startLoad=function(){var a=this;a.options.arrows===!0&&a.slideCount>a.options.slidesToShow&&(a.$prevArrow.hide(),a.$nextArrow.hide()),a.options.dots===!0&&a.slideCount>a.options.slidesToShow&&a.$dots.hide(),a.$slider.addClass("slick-loading")},b.prototype.swipeDirection=function(){var a,b,c,d,e=this;return a=e.touchObject.startX-e.touchObject.curX,b=e.touchObject.startY-e.touchObject.curY,c=Math.atan2(b,a),d=Math.round(180*c/Math.PI),0>d&&(d=360-Math.abs(d)),45>=d&&d>=0?e.options.rtl===!1?"left":"right":360>=d&&d>=315?e.options.rtl===!1?"left":"right":d>=135&&225>=d?e.options.rtl===!1?"right":"left":e.options.verticalSwiping===!0?d>=35&&135>=d?"left":"right":"vertical"},b.prototype.swipeEnd=function(){var c,b=this;if(b.dragging=!1,b.shouldClick=b.touchObject.swipeLength>10?!1:!0,void 0===b.touchObject.curX)return!1;if(b.touchObject.edgeHit===!0&&b.$slider.trigger("edge",[b,b.swipeDirection()]),b.touchObject.swipeLength>=b.touchObject.minSwipe)switch(b.swipeDirection()){case"left":c=b.options.swipeToSlide?b.checkNavigable(b.currentSlide+b.getSlideCount()):b.currentSlide+b.getSlideCount(),b.slideHandler(c),b.currentDirection=0,b.touchObject={},b.$slider.trigger("swipe",[b,"left"]);break;case"right":c=b.options.swipeToSlide?b.checkNavigable(b.currentSlide-b.getSlideCount()):b.currentSlide-b.getSlideCount(),b.slideHandler(c),b.currentDirection=1,b.touchObject={},b.$slider.trigger("swipe",[b,"right"])}else b.touchObject.startX!==b.touchObject.curX&&(b.slideHandler(b.currentSlide),b.touchObject={})},b.prototype.swipeHandler=function(a){var b=this;if(!(b.options.swipe===!1||"ontouchend"in document&&b.options.swipe===!1||b.options.draggable===!1&&-1!==a.type.indexOf("mouse")))switch(b.touchObject.fingerCount=a.originalEvent&&void 0!==a.originalEvent.touches?a.originalEvent.touches.length:1,b.touchObject.minSwipe=b.listWidth/b.options.touchThreshold,b.options.verticalSwiping===!0&&(b.touchObject.minSwipe=b.listHeight/b.options.touchThreshold),a.data.action){case"start":b.swipeStart(a);break;case"move":b.swipeMove(a);break;case"end":b.swipeEnd(a)}},b.prototype.swipeMove=function(a){var d,e,f,g,h,b=this;return h=void 0!==a.originalEvent?a.originalEvent.touches:null,!b.dragging||h&&1!==h.length?!1:(d=b.getLeft(b.currentSlide),b.touchObject.curX=void 0!==h?h[0].pageX:a.clientX,b.touchObject.curY=void 0!==h?h[0].pageY:a.clientY,b.touchObject.swipeLength=Math.round(Math.sqrt(Math.pow(b.touchObject.curX-b.touchObject.startX,2))),b.options.verticalSwiping===!0&&(b.touchObject.swipeLength=Math.round(Math.sqrt(Math.pow(b.touchObject.curY-b.touchObject.startY,2)))),e=b.swipeDirection(),"vertical"!==e?(void 0!==a.originalEvent&&b.touchObject.swipeLength>4&&a.preventDefault(),g=(b.options.rtl===!1?1:-1)*(b.touchObject.curX>b.touchObject.startX?1:-1),b.options.verticalSwiping===!0&&(g=b.touchObject.curY>b.touchObject.startY?1:-1),f=b.touchObject.swipeLength,b.touchObject.edgeHit=!1,b.options.infinite===!1&&(0===b.currentSlide&&"right"===e||b.currentSlide>=b.getDotCount()&&"left"===e)&&(f=b.touchObject.swipeLength*b.options.edgeFriction,b.touchObject.edgeHit=!0),b.swipeLeft=b.options.vertical===!1?d+f*g:d+f*(b.$list.height()/b.listWidth)*g,b.options.verticalSwiping===!0&&(b.swipeLeft=d+f*g),b.options.fade===!0||b.options.touchMove===!1?!1:b.animating===!0?(b.swipeLeft=null,!1):(b.setCSS(b.swipeLeft),void 0)):void 0)},b.prototype.swipeStart=function(a){var c,b=this;return 1!==b.touchObject.fingerCount||b.slideCount<=b.options.slidesToShow?(b.touchObject={},!1):(void 0!==a.originalEvent&&void 0!==a.originalEvent.touches&&(c=a.originalEvent.touches[0]),b.touchObject.startX=b.touchObject.curX=void 0!==c?c.pageX:a.clientX,b.touchObject.startY=b.touchObject.curY=void 0!==c?c.pageY:a.clientY,b.dragging=!0,void 0)},b.prototype.unfilterSlides=b.prototype.slickUnfilter=function(){var a=this;null!==a.$slidesCache&&(a.unload(),a.$slideTrack.children(this.options.slide).detach(),a.$slidesCache.appendTo(a.$slideTrack),a.reinit())},b.prototype.unload=function(){var b=this;a(".slick-cloned",b.$slider).remove(),b.$dots&&b.$dots.remove(),b.$prevArrow&&b.htmlExpr.test(b.options.prevArrow)&&b.$prevArrow.remove(),b.$nextArrow&&b.htmlExpr.test(b.options.nextArrow)&&b.$nextArrow.remove(),b.$slides.removeClass("slick-slide slick-active slick-visible slick-current").attr("aria-hidden","true").css("width","")},b.prototype.unslick=function(a){var b=this;b.$slider.trigger("unslick",[b,a]),b.destroy()},b.prototype.updateArrows=function(){var b,a=this;b=Math.floor(a.options.slidesToShow/2),a.options.arrows===!0&&a.slideCount>a.options.slidesToShow&&!a.options.infinite&&(a.$prevArrow.removeClass("slick-disabled").attr("aria-disabled","false"),a.$nextArrow.removeClass("slick-disabled").attr("aria-disabled","false"),0===a.currentSlide?(a.$prevArrow.addClass("slick-disabled").attr("aria-disabled","true"),a.$nextArrow.removeClass("slick-disabled").attr("aria-disabled","false")):a.currentSlide>=a.slideCount-a.options.slidesToShow&&a.options.centerMode===!1?(a.$nextArrow.addClass("slick-disabled").attr("aria-disabled","true"),a.$prevArrow.removeClass("slick-disabled").attr("aria-disabled","false")):a.currentSlide>=a.slideCount-1&&a.options.centerMode===!0&&(a.$nextArrow.addClass("slick-disabled").attr("aria-disabled","true"),a.$prevArrow.removeClass("slick-disabled").attr("aria-disabled","false")))},b.prototype.updateDots=function(){var a=this;null!==a.$dots&&(a.$dots.find("li").removeClass("slick-active").attr("aria-hidden","true"),a.$dots.find("li").eq(Math.floor(a.currentSlide/a.options.slidesToScroll)).addClass("slick-active").attr("aria-hidden","false"))},b.prototype.visibility=function(){var a=this;document[a.hidden]?(a.paused=!0,a.autoPlayClear()):a.options.autoplay===!0&&(a.paused=!1,a.autoPlay())},b.prototype.initADA=function(){var b=this;b.$slides.add(b.$slideTrack.find(".slick-cloned")).attr({"aria-hidden":"true",tabindex:"-1"}).find("a, input, button, select").attr({tabindex:"-1"}),b.$slideTrack.attr("role","listbox"),b.$slides.not(b.$slideTrack.find(".slick-cloned")).each(function(c){a(this).attr({role:"option","aria-describedby":"slick-slide"+b.instanceUid+c})}),null!==b.$dots&&b.$dots.attr("role","tablist").find("li").each(function(c){a(this).attr({role:"presentation","aria-selected":"false","aria-controls":"navigation"+b.instanceUid+c,id:"slick-slide"+b.instanceUid+c})}).first().attr("aria-selected","true").end().find("button").attr("role","button").end().closest("div").attr("role","toolbar"),b.activateADA()},b.prototype.activateADA=function(){var a=this,b=a.$slider.find("*").is(":focus");a.$slideTrack.find(".slick-active").attr({"aria-hidden":"false",tabindex:"0"}).find("a, input, button, select").attr({tabindex:"0"}),b&&a.$slideTrack.find(".slick-active").focus()},b.prototype.focusHandler=function(){var b=this;b.$slider.on("focus.slick blur.slick","*",function(c){c.stopImmediatePropagation();var d=a(this);setTimeout(function(){b.isPlay&&(d.is(":focus")?(b.autoPlayClear(),b.paused=!0):(b.paused=!1,b.autoPlay()))},0)})},a.fn.slick=function(){var g,a=this,c=arguments[0],d=Array.prototype.slice.call(arguments,1),e=a.length,f=0;for(f;e>f;f++)if("object"==typeof c||"undefined"==typeof c?a[f].slick=new b(a[f],c):g=a[f].slick[c].apply(a[f].slick,d),"undefined"!=typeof g)return g;return a}});
 $(document).ready(function() {
 
-	winnerTopList = [{"winnerName":"Test T.","currencySymbol":"$","winAmount":"5850.00","timestamp":"17/11 - 11:58 GMT","gameName":"Millionaire Club 3","gameLogoSmall":"//cdn.jackiejackpot.com/games/fallbackSmallGameLogo.jpg","gameLogoBig":"//cdn.jackiejackpot.com/games/fallbackBigGameLogo.jpg","countryCode":"GB","currencyFactor":1,"currencyAlignment":"L"},{"winnerName":"Andreas C.","currencySymbol":"£","winAmount":"5128.00","timestamp":"17/11 - 22:47 GMT","gameName":"Foxin wins again","gameLogoSmall":"//cdn.jackiejackpot.com/games/fallbackSmallGameLogo.jpg","gameLogoBig":"//cdn.jackiejackpot.com/games/fallbackBigGameLogo.jpg","countryCode":"GB","currencyFactor":1,"currencyAlignment":"L"},{"winnerName":"Lisa C.","currencySymbol":"$","winAmount":"4540.00","timestamp":"18/11 - 02:02 GMT","gameName":"Reel Rush","gameLogoSmall":"//cdn.jackiejackpot.com/games/fallbackSmallGameLogo.jpg","gameLogoBig":"//cdn.jackiejackpot.com/games/fallbackBigGameLogo.jpg","countryCode":"AU","currencyFactor":1,"currencyAlignment":"L"},{"winnerName":"Tero H.","currencySymbol":"?","winAmount":"4501.00","timestamp":"17/11 - 13:07 GMT","gameName":"Gonzo\u0027s Quest","gameLogoSmall":"//cdn.jackiejackpot.com/games/fallbackSmallGameLogo.jpg","gameLogoBig":"//cdn.jackiejackpot.com/games/fallbackBigGameLogo.jpg","countryCode":"FI","currencyFactor":1,"currencyAlignment":"L"},{"winnerName":"Sami L.","currencySymbol":"?","winAmount":"3061.00","timestamp":"17/11 - 10:38 GMT","gameName":"Steam Tower","gameLogoSmall":"//cdn.jackiejackpot.com/games/fallbackSmallGameLogo.jpg","gameLogoBig":"//cdn.jackiejackpot.com/games/fallbackBigGameLogo.jpg","countryCode":"FI","currencyFactor":1,"currencyAlignment":"L"},{"winnerName":"Maroun E.","currencySymbol":"$","winAmount":"2770.50","timestamp":"17/11 - 14:28 GMT","gameName":"Jack and the Beanstalk","gameLogoSmall":"//cdn.jackiejackpot.com/games/fallbackSmallGameLogo.jpg","gameLogoBig":"//cdn.jackiejackpot.com/games/fallbackBigGameLogo.jpg","countryCode":"AE","currencyFactor":1,"currencyAlignment":"L"},{"winnerName":"Nina J.","currencySymbol":"kr","winAmount":"26070.00","timestamp":"17/11 - 20:47 GMT","gameName":"Gonzo\u0027s Quest","gameLogoSmall":"//cdn.jackiejackpot.com/games/fallbackSmallGameLogo.jpg","gameLogoBig":"//cdn.jackiejackpot.com/games/fallbackBigGameLogo.jpg","countryCode":"NO","currencyFactor":10,"currencyAlignment":"R"},{"winnerName":"Rahim M.","currencySymbol":"kr","winAmount":"25330.00","timestamp":"17/11 - 23:44 GMT","gameName":"FUNtastic Pets","gameLogoSmall":"//cdn.jackiejackpot.com/games/fallbackSmallGameLogo.jpg","gameLogoBig":"//cdn.jackiejackpot.com/games/fallbackBigGameLogo.jpg","countryCode":"SE","currencyFactor":10,"currencyAlignment":"R"},{"winnerName":"Nadia V.","currencySymbol":"?","winAmount":"2435.00","timestamp":"17/11 - 23:35 GMT","gameName":"Reel Rush","gameLogoSmall":"//cdn.jackiejackpot.com/games/fallbackSmallGameLogo.jpg","gameLogoBig":"//cdn.jackiejackpot.com/games/fallbackBigGameLogo.jpg","countryCode":"FR","currencyFactor":1,"currencyAlignment":"L"},{"winnerName":"Pascal R.","currencySymbol":"?","winAmount":"2038.00","timestamp":"17/11 - 17:51 GMT","gameName":"Subtopia","gameLogoSmall":"//cdn.jackiejackpot.com/games/fallbackSmallGameLogo.jpg","gameLogoBig":"//cdn.jackiejackpot.com/games/fallbackBigGameLogo.jpg","countryCode":"FR","currencyFactor":1,"currencyAlignment":"L"}];
-	var html = '';
-	// console.log(winnerTopList);
-	$.each(winnerTopList, function(index) {
-	    // console.log(winnerTopList[index].gameLogoSmall);
-	    html += '<div class="promo__slide"><div class="promo__icon">';
-	    html += '<img src="http:'+winnerTopList[index].gameLogoSmall+'" alt="">';
-	    html += '</div>';
-	    html += '<div class="promo__text">';
-	    html += '<div class="promo__price">'+winnerTopList[index].winAmount+'</div>';
-	    html += '<h3 class="promo__title">'+winnerTopList[index].winnerName+'</h3>';
-	    html += '<span class="promo__date">'+winnerTopList[index].timestamp+'</span>';
-	    html += '<div class="promo__game">'+winnerTopList[index].gameName+'</div>';
-	    html += '</div></div>';
+
+	$.getJSON( "/winnerslast10.json", function( winnerTopList ) {
+		var html = '';
+		$.each(winnerTopList, function(index) {
+		    // console.log(winnerTopList[index].gameLogoSmall);
+		    html += '<div class="promo__slide"><div class="promo__icon" style="background-image: url('+winnerTopList[index].gameLogoSmall+');">';
+		    html += '</div>';
+		    html += '<div class="promo__text">';
+		    html += '<div class="promo__price">'+winnerTopList[index].winAmount+'</div>';
+		    html += '<h3 class="promo__title">'+winnerTopList[index].winnerName+'</h3>';
+		    html += '<span class="promo__date">'+winnerTopList[index].timestamp+'</span>';
+		    html += '<div class="promo__game">'+winnerTopList[index].gameName+'</div>';
+		    html += '</div></div>';
+		});
+		// console.log(html);
+			$('.js-promo').html(html);
+			$('.js-promo').slick({
+				slidesToShow: 3,
+			  	slidesToScroll: 1,
+			  	infinite: true,
+				arrows: true,
+				dots: false,
+				prevArrow: $('.promo__prev'),
+				nextArrow: $('.promo__next'),
+				autoplay: true,
+		  		autoplaySpeed: 2000
+			})
 	});
-	// console.log(html);
-	$('.js-promo').html(html);
-
-
-
+	
 	// latest winner 7 days, 30 days
 	$.getJSON( "/winnersLast7days.json", function( data ) {
-		console.log(data);
-	  	putjson(data,'weekly')
+	  	putjson(data,'weekly');
+	  		
 	});
 	$.getJSON( "/winnersLast30days.json", function( data ) {
 	  	putjson(data,'yearly')
@@ -75,7 +84,21 @@ $(document).ready(function() {
 			html += '	</div>';
 			html += '</div>';
 		});
-		$('#'+id).html(html);
+		$b = $('#'+id);
+		$prev = $b.parent().find('.jacks__top');
+		$next = $b.parent().find('.jacks__more');
+		$b.html(html).slick({
+	  			slidesToShow: 4,
+	  		  	slidesToScroll: 1,
+	  		  	vertical: true,
+	  		  	infinite: true,
+	  			autoplay: true,
+	  	  		autoplaySpeed: 3000,
+	  	  		speed: 1000,
+	  	  		prevArrow: $prev,
+	  	  		nextArrow: $next,
+	  	  		slide: '.jack'
+	  		});
 	}
 
 
@@ -90,11 +113,11 @@ $(document).ready(function() {
 		/* Stuff to do when the mouse leaves the element */
 	});
 
-	$('.jacks__more').click(function(event) {
-		$(this).hide();
-		$(this).parent().find('.is-hidden').show();
-		return false;
-	});
+	// $('.jacks__more').click(function(event) {
+	// 	$(this).hide();
+	// 	$(this).parent().find('.is-hidden').show();
+	// 	return false;
+	// });
 	// click(function(event) {
 	// 	$('.header__drop').toggle();
 	// });
@@ -110,19 +133,11 @@ $(document).ready(function() {
   		autoplaySpeed: 5000,
   		fade: true,
   		cssEase: 'linear',
-  		 speed: 1000,
+  		speed: 600,
 	})
-	$('.js-promo').slick({
-		slidesToShow: 3,
-	  	slidesToScroll: 1,
-	  	infinite: true,
-		arrows: true,
-		dots: false,
-		prevArrow: $('.promo__prev'),
-		nextArrow: $('.promo__next'),
-		autoplay: true,
-  		autoplaySpeed: 2000
-	})
+
+		
+	
 	$('.gnav__section').click(function(event) {
 		$(this).siblings().removeClass('is-active');
 		$(this).addClass('is-active');
--- site/js/common.js
+++ site/js/common.js
@@ -1,29 +1,38 @@
 $(document).ready(function() {
 
-	winnerTopList = [{"winnerName":"Test T.","currencySymbol":"$","winAmount":"5850.00","timestamp":"17/11 - 11:58 GMT","gameName":"Millionaire Club 3","gameLogoSmall":"//cdn.jackiejackpot.com/games/fallbackSmallGameLogo.jpg","gameLogoBig":"//cdn.jackiejackpot.com/games/fallbackBigGameLogo.jpg","countryCode":"GB","currencyFactor":1,"currencyAlignment":"L"},{"winnerName":"Andreas C.","currencySymbol":"£","winAmount":"5128.00","timestamp":"17/11 - 22:47 GMT","gameName":"Foxin wins again","gameLogoSmall":"//cdn.jackiejackpot.com/games/fallbackSmallGameLogo.jpg","gameLogoBig":"//cdn.jackiejackpot.com/games/fallbackBigGameLogo.jpg","countryCode":"GB","currencyFactor":1,"currencyAlignment":"L"},{"winnerName":"Lisa C.","currencySymbol":"$","winAmount":"4540.00","timestamp":"18/11 - 02:02 GMT","gameName":"Reel Rush","gameLogoSmall":"//cdn.jackiejackpot.com/games/fallbackSmallGameLogo.jpg","gameLogoBig":"//cdn.jackiejackpot.com/games/fallbackBigGameLogo.jpg","countryCode":"AU","currencyFactor":1,"currencyAlignment":"L"},{"winnerName":"Tero H.","currencySymbol":"?","winAmount":"4501.00","timestamp":"17/11 - 13:07 GMT","gameName":"Gonzo\u0027s Quest","gameLogoSmall":"//cdn.jackiejackpot.com/games/fallbackSmallGameLogo.jpg","gameLogoBig":"//cdn.jackiejackpot.com/games/fallbackBigGameLogo.jpg","countryCode":"FI","currencyFactor":1,"currencyAlignment":"L"},{"winnerName":"Sami L.","currencySymbol":"?","winAmount":"3061.00","timestamp":"17/11 - 10:38 GMT","gameName":"Steam Tower","gameLogoSmall":"//cdn.jackiejackpot.com/games/fallbackSmallGameLogo.jpg","gameLogoBig":"//cdn.jackiejackpot.com/games/fallbackBigGameLogo.jpg","countryCode":"FI","currencyFactor":1,"currencyAlignment":"L"},{"winnerName":"Maroun E.","currencySymbol":"$","winAmount":"2770.50","timestamp":"17/11 - 14:28 GMT","gameName":"Jack and the Beanstalk","gameLogoSmall":"//cdn.jackiejackpot.com/games/fallbackSmallGameLogo.jpg","gameLogoBig":"//cdn.jackiejackpot.com/games/fallbackBigGameLogo.jpg","countryCode":"AE","currencyFactor":1,"currencyAlignment":"L"},{"winnerName":"Nina J.","currencySymbol":"kr","winAmount":"26070.00","timestamp":"17/11 - 20:47 GMT","gameName":"Gonzo\u0027s Quest","gameLogoSmall":"//cdn.jackiejackpot.com/games/fallbackSmallGameLogo.jpg","gameLogoBig":"//cdn.jackiejackpot.com/games/fallbackBigGameLogo.jpg","countryCode":"NO","currencyFactor":10,"currencyAlignment":"R"},{"winnerName":"Rahim M.","currencySymbol":"kr","winAmount":"25330.00","timestamp":"17/11 - 23:44 GMT","gameName":"FUNtastic Pets","gameLogoSmall":"//cdn.jackiejackpot.com/games/fallbackSmallGameLogo.jpg","gameLogoBig":"//cdn.jackiejackpot.com/games/fallbackBigGameLogo.jpg","countryCode":"SE","currencyFactor":10,"currencyAlignment":"R"},{"winnerName":"Nadia V.","currencySymbol":"?","winAmount":"2435.00","timestamp":"17/11 - 23:35 GMT","gameName":"Reel Rush","gameLogoSmall":"//cdn.jackiejackpot.com/games/fallbackSmallGameLogo.jpg","gameLogoBig":"//cdn.jackiejackpot.com/games/fallbackBigGameLogo.jpg","countryCode":"FR","currencyFactor":1,"currencyAlignment":"L"},{"winnerName":"Pascal R.","currencySymbol":"?","winAmount":"2038.00","timestamp":"17/11 - 17:51 GMT","gameName":"Subtopia","gameLogoSmall":"//cdn.jackiejackpot.com/games/fallbackSmallGameLogo.jpg","gameLogoBig":"//cdn.jackiejackpot.com/games/fallbackBigGameLogo.jpg","countryCode":"FR","currencyFactor":1,"currencyAlignment":"L"}];
-	var html = '';
-	// console.log(winnerTopList);
-	$.each(winnerTopList, function(index) {
-	    // console.log(winnerTopList[index].gameLogoSmall);
-	    html += '<div class="promo__slide"><div class="promo__icon">';
-	    html += '<img src="http:'+winnerTopList[index].gameLogoSmall+'" alt="">';
-	    html += '</div>';
-	    html += '<div class="promo__text">';
-	    html += '<div class="promo__price">'+winnerTopList[index].winAmount+'</div>';
-	    html += '<h3 class="promo__title">'+winnerTopList[index].winnerName+'</h3>';
-	    html += '<span class="promo__date">'+winnerTopList[index].timestamp+'</span>';
-	    html += '<div class="promo__game">'+winnerTopList[index].gameName+'</div>';
-	    html += '</div></div>';
+
+	$.getJSON( "/winnerslast10.json", function( winnerTopList ) {
+		var html = '';
+		$.each(winnerTopList, function(index) {
+		    // console.log(winnerTopList[index].gameLogoSmall);
+		    html += '<div class="promo__slide"><div class="promo__icon" style="background-image: url('+winnerTopList[index].gameLogoSmall+');">';
+		    html += '</div>';
+		    html += '<div class="promo__text">';
+		    html += '<div class="promo__price">'+winnerTopList[index].winAmount+'</div>';
+		    html += '<h3 class="promo__title">'+winnerTopList[index].winnerName+'</h3>';
+		    html += '<span class="promo__date">'+winnerTopList[index].timestamp+'</span>';
+		    html += '<div class="promo__game">'+winnerTopList[index].gameName+'</div>';
+		    html += '</div></div>';
+		});
+		// console.log(html);
+			$('.js-promo').html(html);
+			$('.js-promo').slick({
+				slidesToShow: 3,
+			  	slidesToScroll: 1,
+			  	infinite: true,
+				arrows: true,
+				dots: false,
+				prevArrow: $('.promo__prev'),
+				nextArrow: $('.promo__next'),
+				autoplay: true,
+		  		autoplaySpeed: 2000
+			})
 	});
-	// console.log(html);
-	$('.js-promo').html(html);
-
-
-
+	
 	// latest winner 7 days, 30 days
 	$.getJSON( "/winnersLast7days.json", function( data ) {
-		console.log(data);
-	  	putjson(data,'weekly')
+	  	putjson(data,'weekly');
+	  		
 	});
 	$.getJSON( "/winnersLast30days.json", function( data ) {
 	  	putjson(data,'yearly')
@@ -52,7 +61,21 @@ $(document).ready(function() {
 			html += '	</div>';
 			html += '</div>';
 		});
-		$('#'+id).html(html);
+		$b = $('#'+id);
+		$prev = $b.parent().find('.jacks__top');
+		$next = $b.parent().find('.jacks__more');
+		$b.html(html).slick({
+	  			slidesToShow: 4,
+	  		  	slidesToScroll: 1,
+	  		  	vertical: true,
+	  		  	infinite: true,
+	  			autoplay: true,
+	  	  		autoplaySpeed: 3000,
+	  	  		speed: 1000,
+	  	  		prevArrow: $prev,
+	  	  		nextArrow: $next,
+	  	  		slide: '.jack'
+	  		});
 	}
 
 
@@ -67,11 +90,11 @@ $(document).ready(function() {
 		/* Stuff to do when the mouse leaves the element */
 	});
 
-	$('.jacks__more').click(function(event) {
-		$(this).hide();
-		$(this).parent().find('.is-hidden').show();
-		return false;
-	});
+	// $('.jacks__more').click(function(event) {
+	// 	$(this).hide();
+	// 	$(this).parent().find('.is-hidden').show();
+	// 	return false;
+	// });
 	// click(function(event) {
 	// 	$('.header__drop').toggle();
 	// });
@@ -87,19 +110,11 @@ $(document).ready(function() {
   		autoplaySpeed: 5000,
   		fade: true,
   		cssEase: 'linear',
-  		 speed: 1000,
+  		speed: 600,
 	})
-	$('.js-promo').slick({
-		slidesToShow: 3,
-	  	slidesToScroll: 1,
-	  	infinite: true,
-		arrows: true,
-		dots: false,
-		prevArrow: $('.promo__prev'),
-		nextArrow: $('.promo__next'),
-		autoplay: true,
-  		autoplaySpeed: 2000
-	})
+
+		
+	
 	$('.gnav__section').click(function(event) {
 		$(this).siblings().removeClass('is-active');
 		$(this).addClass('is-active');
